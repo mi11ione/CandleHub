a30da05cc2d4cc29c631cbb85cde49311895bf07
mi11ione <i@mi11ion.io>
2024-03-19 16:45:43 +0300

    Initial Commit

---
+++ CandleHub/CandleHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandleHubApp.swift
+//  CandleHub
+//
+//  Created by mi11ion on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CandleHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
