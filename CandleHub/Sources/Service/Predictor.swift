1b04c7c2b141afe8e86b7bd596b96400153e6dd7
koshkar <qwerty376l@mail.ru>
2024-03-22 16:38:24 +0300

    some architect fixes

---
+++ CandleHub/Sources/Service/Predictor.swift
@@ -0,0 +1,40 @@
+//
+//  Model.swift
+//  CandleHub
+//
+//  Created by Кирилл Кошкарёв on 22.03.2024.
+//
+
+import CoreML
+import SwiftUI
+
+struct CandlePredictor: Predictor {
+    
+    func predict(modelInput: OpenRegressionModelInput) ->  Double {
+        guard let modelURL = Bundle.main.url(forResource: "OpenRegressionModel", withExtension: "mlmodelc") else {
+            fatalError("Failed to find YourModel.mlmodel in main bundle")
+        }
+        
+        do {
+            
+            let mlModel = try MLModel(contentsOf: modelURL)
+            let prediction = try mlModel.prediction(from: modelInput)
+            
+            if let doublePrediction = prediction.featureValue(for: "open_awaited")?.doubleValue {
+                return doublePrediction
+            } else {
+                fatalError("Failed to get double prediction from ML model")
+            }
+        } catch {
+            fatalError("Error making prediction: \(error.localizedDescription)")
+        }
+        
+    }
+    
+}
+
+protocol Predictor {
+    
+    func predict(modelInput: OpenRegressionModelInput) -> Double
+    
+}
