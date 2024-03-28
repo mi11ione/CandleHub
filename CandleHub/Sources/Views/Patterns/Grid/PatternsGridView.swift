ebf75d2daa742061ddc5f6c7e075f0678d4ad984
mi11ione <i@mi11ion.io>
2024-03-28 13:27:16 +0300

    heartstone

--- CandleHub/Sources/Views/Patterns/Grid/PatternsGridView.swift
+++ CandleHub/Sources/Views/Patterns/Grid/PatternsGridView.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
 struct PatternsGridView: View {
-    @Environment(\.colorScheme) var colorScheme
     var patterns: [Pattern]
     @Binding var selectedOption: Option
     @State private var selectedPattern: Pattern?
+    var viewModel: PatternsGridViewModel
 
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 20) {
+        LazyVGrid(columns: viewModel.adaptiveColumn(for: selectedOption), spacing: 20) {
             ForEach(patterns, id: \.id) { pattern in
                 VStack {
-                    PatternStickChart(pattern: pattern, gridWidth: gridWidth(for: selectedOption))
+                    PatternStickChart(pattern: pattern, gridWidth: viewModel.gridWidth(for: selectedOption))
                         .background(Material.thin)
                         .cornerRadius(30)
 
@@ -26,22 +26,5 @@ struct PatternsGridView: View {
             }
         }
         .padding([.top, .horizontal])
-    }
-
-    var columns: [GridItem] {
-        [GridItem(.adaptive(minimum: gridWidth(for: selectedOption)))]
-    }
-
-    func gridWidth(for selectedOption: Option) -> CGFloat {
-        switch selectedOption {
-        case .bigPatterns:
-            350
-        case .smallPatterns:
-            160
-        }
-    }
-
-    func adaptiveColumn(for selectedOption: Option) -> [GridItem] {
-        [GridItem(.adaptive(minimum: gridWidth(for: selectedOption)))]
     }
 }
